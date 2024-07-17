b4a0cf7fd1ce0df018076f365b642af22ba7ff18
Squirrel68 <constantlypeaceinside@gmail.com>
2024-07-17 16:04:50 +0700

    #1 - 5-5.1-sign-up-shop-use-rsa

---
+++ src/models/keytoken.model.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const { Schema, model } = require("mongoose");
+
+const DOCUMENT_NAME = "Key";
+const COLLECTION_NAME = "Keys";
+
+// Declare the Schema of the Mongo model
+var keyTokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Shop",
+    },
+    publicKey: {
+      type: String,
+      require: true,
+    },
+    refreshToken: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+// Export the module
+module.exports = model(DOCUMENT_NAME, keyTokenSchema);
