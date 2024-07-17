f35a370dc755ad71ad88e90da33ddcaa54309034
Squirrel68 <constantlypeaceinside@gmail.com>
2024-07-17 08:19:30 +0700

    #1 - 3-connect-mongo-to-node

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-process.on("unhandledRejection", (err, promise) => {
+process.on("SIGINT", () => {
   server.close(() => console.log(`Exit server express`));
 });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const compression = require("compression");
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const { checkOverload } = require("./helpers/check.connect");
 const app = express();
 
 // innit middleware
@@ -9,13 +10,17 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 // init db
+require("./dbs/init.mongodb");
+checkOverload();
 
 // init routes
 
 // handling error
 
-app.get("/", (req, res) => {
-  res.send("Hello World !!!");
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello world",
+  });
 });
 
 module.exports = app;
