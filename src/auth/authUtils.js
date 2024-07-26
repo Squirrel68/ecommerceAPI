ba6bf64f6796ea386ab56f12b697ebe209b2279b
Squirrel68 <constantlypeaceinside@gmail.com>
2024-07-26 10:14:21 +0700

    fix: delete algoithm rs256

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -1,28 +1,27 @@
-"use strict";
+'use strict'
 
-const JWT = require("jsonwebtoken");
+const JWT = require('jsonwebtoken')
 
 const createTokenPair = async (payload, publicKey, privateKey) => {
   try {
     const accessToken = await JWT.sign(payload, publicKey, {
-      algorithm: "RS256",
-      expiresIn: "2 days",
-    });
+      expiresIn: '2 days'
+    })
     const refreshToken = await JWT.sign(payload, privateKey, {
-      expiresIn: "7 days",
-    });
+      expiresIn: '7 days'
+    })
 
     JWT.verify(accessToken, publicKey, (err, decode) => {
       if (err) {
-        console.error(`error verify::`, err);
+        console.error(`error verify::`, err)
       } else {
-        console.log(`decode verify::`, decode);
+        console.log(`decode verify::`, decode)
       }
-    });
-    return { accessToken, refreshToken };
+    })
+    return { accessToken, refreshToken }
   } catch (error) {}
-};
+}
 
 module.exports = {
-  createTokenPair,
-};
+  createTokenPair
+}
